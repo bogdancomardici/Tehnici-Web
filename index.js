720201a4305b717c7360c56abb0128af2d7c06a0
bogdancomardici <115560133+bogdancomardici@users.noreply.github.com>
2023-03-20 12:51:09 +0200

    grouped files in folders

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const express = require("express");
-
+const fs = require("fs");
 app = express();
+app.set("view engine", "ejs");
 
 console.log("Folder proiect:", __dirname);
 
 app.use("/resurse", express.static(__dirname + "/resurse"));
-app.get("/ceva", function(req, res){
+app.get("/ceva", function (req, res) {
     res.send("altceva");
 });
 
-app.get(["/index", "/", "/home"], function(req, res){
+app.get(["/index", "/", "/home"], function (req, res) {
     console.log(req.ip);
-    res.render("pagini/index.ejs", {ip: req.ip});
-    
+    res.render("pagini/index.ejs", { ip: req.ip });
+
 });
 
-app.get("/despre", function(req, res){
-    res.render("pagini/despre.ejs");
+app.get("/*", function (req, res) {
+    res.render("pagini" + req.url, function (err, rezRandare) {
+        if (err) {
+            console.log(err);
+            res.send("error");
+        }
+        else {
+            console.log(rezRandare);
+            res.send(rezRandare);
+        }
+    });
 });
+
+function initErori() {
+
+    var continut = fs.readFileSync(__dirname + "/resurse/json/erori.json").toString("utf-8");
+    obErori = JSON.parse(continut);
+}
 
 app.listen(8080);
 
 console.log("Serverul a pornit!");
+
