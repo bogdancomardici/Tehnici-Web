4a20e64e0b6933aa6a5d0d2e92d07a64fae6ba72
bogdancomardici <115560133+bogdancomardici@users.noreply.github.com>
2023-03-20 09:55:53 +0200

    added ejs

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ app = express();
 
 console.log("Folder proiect:", __dirname);
 
+app.use("/resurse", express.static(__dirname + "/resurse"));
 app.get("/ceva", function(req, res){
     res.send("altceva");
 });
 
-app.get("/index.html", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+app.get(["/index", "/", "/home"], function(req, res){
+    console.log(req.ip);
+    res.render("pagini/index.ejs", {ip: req.ip});
+    
+});
+
+app.get("/despre", function(req, res){
+    res.render("pagini/despre.ejs");
 });
 
 app.listen(8080);
