5768f9972de14acf3c434c55e20308e0e1dc446b
bogdancomardici <115560133+bogdancomardici@users.noreply.github.com>
2023-03-27 09:53:49 +0300

    added errors

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const fs = require("fs");
+
+obGlobal = {
+    obErori: null,
+    obImagini: null,
+};
 app = express();
 app.set("view engine", "ejs");
 
 console.log("Folder proiect:", __dirname);
+console.log("Cale fisier:", __filename);
+console.log("Director de lucru:", process.cwd());
 
 app.use("/resurse", express.static(__dirname + "/resurse"));
+
+app.use(/\/resurse(\/((?=[0-9])|(?=[a-z])|(?=[A-Z])))*/, function (req, res) {
+    afisareEroare(res, 403);
+});
+
+// 01ABCD".match(/^[0-9A-Fa-f]+$/)
 app.get("/ceva", function (req, res) {
     res.send("altceva");
 });
@@ -19,12 +32,13 @@ app.get(["/index", "/", "/home"], function (req, res) {
 app.get("/*", function (req, res) {
     res.render("pagini" + req.url, function (err, rezRandare) {
         if (err) {
-            console.log(err);
-            res.send("error");
+            if (err.message.startsWith("Failed to lookup view")) {
+                // afiseazaEroare(res, { _identificator: 404, _titlu: "Pagina nu a fost gasita", _text: "Pagina nu a fost gasita", _imagine: "/resurse/images/erori/lupa.jpg" });
+                afiseazaEroare(res, 404);
+            }
         }
         else {
-            console.log(rezRandare);
-            res.send(rezRandare);
+            afiseazaEroare(res);
         }
     });
 });
@@ -32,9 +46,49 @@ app.get("/*", function (req, res) {
 function initErori() {
 
     var continut = fs.readFileSync(__dirname + "/resurse/json/erori.json").toString("utf-8");
-    obErori = JSON.parse(continut);
+    obGlobal.obErori = JSON.parse(continut);
+
+    let vErori = obGlobal.obErori.info_erori;
+    // for(let i = 0; i < obGlobal.obErori.info_erori.length; i++) {
+    //     console.log(vErori[i].imagine);
+
+    for (let eroare of vErori) {
+        eroare.imagine = "/" + obGlobal.obErori.cale_baza + "/" + eroare.imagine;
+        console.log(eroare.imagine);
+    }
 }
 
+// daca  programatorul seteaza titlul, se ia titlul din argument
+// daca nu e setat, se ia cel din json
+// daca nu avem titlul nici in JSOn se ia titlul de valoarea default
+// idem pentru celelalte
+
+// function afiseazaEroare(res, {_identificator, _titlu, _text, _imagine} = {}) {
+function afiseazaEroare(res, _identificator, _titlu = "titlu default", _text = "text default", _imagine) {
+    let vErori = obGlobal.obErori.info_erori;
+    let eroare = vErori.find(function (element) {
+        return element.identificator === _identificator;
+    });
+
+    if (eroare) {
+        let titlu = _titlu = "titlu default" ? (eroare.titlu || _titlu) : _titlu;
+        let text = _text = "text default" ? (eroare.text || _text) : _text;
+        let imagine = _imagine = "imagine default" ? (eroare.imagine || _imagine) : _imagine;
+        if (eroare.status) {
+            res.status(eroare.identificator).render("pagini/eroare.ejs", { titlu: titlu, text: text, imagine: imagine });
+
+        }
+        else {
+            res.render("pagini/eroare.ejs", { titlu: titlu, text: text, imagine: imagine });
+        }
+    }
+    else {
+        let errDef = obGlobal.obErori.eroare_default;
+        res.render("pagini/eroare.ejs", { titlu: errDef.titlu, text: errDef.text, imagine: obGlobal.obErori.cale_baza + "/" + errDef.imagine });
+    }
+}
+
+initErori();
 app.listen(8080);
 
 console.log("Serverul a pornit!");
